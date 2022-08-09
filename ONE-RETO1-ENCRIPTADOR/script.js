7704e9626838fdb2fd51feb5ac83d348bdc52a73
cahov <86586454+cahov@users.noreply.github.com>
2022-08-09 10:03:44 -0400

    Add files via upload

---
+++ ONE-RETO1-ENCRIPTADOR/script.js
@@ -0,0 +1,35 @@
+function encriptar(){
+    var mensaje = document.getElementById("input").value;
+    var texto = mensaje.replace(/i/igm, "imes");
+    var texto = texto.replace(/a/igm, "ai");
+    var texto = texto.replace(/e/igm, "enter");
+    var texto = texto.replace(/o/igm, "ober");
+    var texto = texto.replace(/u/igm, "ufat");
+    document.getElementById("resultado").innerHTML = texto;
+    document.querySelector('.area-resultado > img').style.display = "none";
+    document.getElementById("input").value = "";
+    document.querySelector('.caja-textos').style.display="none";
+    document.getElementById("resultado").style.display = "block";
+    document.getElementById("copiar").style.display = "inherit";
+}
+
+function desencriptar(){
+    var mensaje = document.getElementById("input").value;
+    var texto = mensaje.replace(/imes/igm, "i");
+    var texto = texto.replace(/ai/igm, "a");
+    var texto = texto.replace(/enter/igm, "e");
+    var texto = texto.replace(/ober/igm, "o");
+    var texto = texto.replace(/ufat/igm, "u");
+    document.getElementById("resultado").innerHTML = texto;
+    document.querySelector('.area-resultado > img').style.display = "none";
+    document.getElementById("input").value = "";
+    document.querySelector('.caja-textos').style.display="none";
+    document.getElementById("resultado").style.display = "block";
+    document.getElementById("copiar").style.display = "inherit";
+}
+
+function copiar(){
+    var texto = document.getElementById("resultado");
+    texto.select();
+    navigator.clipboard.writeText(texto.value);
+}
